447b939069881db6bea956d25a903021b6838672
RubenSystems <ruben.ticehurst-james@kcl.ac.uk>
2023-01-23 10:33:43 +0000

    Block update: - Removed massive template in favour of small templates and generic functions - Added source file to seperate out logic and defs

--- src/include/block.h
+++ src/include/block.h
@@ -24,120 +24,36 @@ enum anyblock_delete_codes {
 	DELETE_SUCCESS
 };
 
-
-static bool block_val_equal(int rhs, int lhs) {
-	return rhs == lhs;
-}
-
-// Placeholder type of type int
-struct internal_block_data_store {
-	int 	data;
+struct block_metadata {
+	uint8_t used_size;
+	uint8_t	capacity;
+	int64_t next;
+	int64_t previous;
+	size_t 	item_size;
 };
 
-struct any_block {
-	struct internal_block_data_store 	data[MAX_COUNT];
-	uint8_t 							used_size;
-	uint8_t 							capacity;
-	int64_t 							next;
-	int64_t 							previous;
-};
-
-struct any_block init_block(void) {
-	struct any_block _init_val = {
-		.used_size = 0,
-		.next = -1,
-		.capacity = MAX_COUNT,
-	};
-	
-	memset(_init_val.data, 0, sizeof(_init_val.data));
-	return _init_val;
-}
-
-enum anyblock_insert_codes insert_into_block(struct any_block * block, struct internal_block_data_store value) {
-	if (block->capacity <= block->used_size && block->next == -1) {
-		return INSERT_NEW_BLOCK;
-	} else if (block->capacity <= block->used_size) {\
-		return INSERT_GOTO_NEXT;
-	} else {
-		block->data[block->used_size++] = value;
-		return INSERT_SUCCESS;
-	}
-}
-
-static void __block_delete_and_shift(struct any_block * block, size_t delete_index) {
-	for (size_t i = delete_index + 1; i < block->used_size; i ++) {
-		block->data[delete_index - 1] = block->data[delete_index];
-	}
-}
-
-enum anyblock_delete_codes delete_from_block(struct any_block * block, struct internal_block_data_store value) {
-	bool del_performed = false;
-	for (int block_index = block->used_size - 1; block_index >= 0; block_index--) {
-		if (block_val_equal(block->data[block_index].data, value.data)) {
-			printf("DELETING\n");
-			__block_delete_and_shift(block, block_index);
-			del_performed = true;
-		}
-	}
-	return del_performed ? DELETE_SUCCESS : DELETE_NOT_FOUND;
-}
-
-
-
-
-// I present to you: Why c++ can sometimes be easier:
-
-
-/*
-#define TYPED_BLOCK_DEF(name, type, max_count)\
-struct name##_block {\
-	type data[max_count];\
-	uint8_t used_size;\
-	uint8_t capacity; \
-	int64_t next;\
-	int64_t previous;\
+#define BLOCK_TYPE(type, name, max_size)\
+struct name {\
+	struct block_metadata	metadata;\
+	type					data[max_size];\
 };\
-\
-struct name##_block init_##name##_block(void);\
-enum anyblock_insert_codes insert_into_##name##_block(struct name##_block * block, type value);\
-enum anyblock_delete_codes delete_from_##name##_block(struct name##_block * block, type value);\
-
-#define TYPED_BLOCK_IMPL(name, type, max_count)\
-struct name##_block init_##name##_block() {\
-	struct name##_block _init_val = {\
-		.used_size = 0,\
-		.capacity = max_count,\
-		.next = -1,\
-		.previous = -1,\
+struct name init_block(void) {\
+	struct name _init_val = {\
+		{\
+			.used_size = 0,\
+			.next = -1,\
+			.previous = -1,\
+			.capacity = max_size,\
+			.item_size = sizeof(type)\
+		}\
 	};\
+	memset(_init_val.data, 0, sizeof(_init_val.data));\
 	return _init_val;\
 }\
-enum anyblock_insert_codes insert_into_##name##_block(struct name##_block * block, type value) {\
-	if (block->capacity <= block->used_size && block->next == -1) {\
-		return INSERT_NEW_BLOCK;\
-	} else if (block->capacity <= block->used_size) {\
-		return INSERT_GOTO_NEXT;\
-	} else {\
-		block->data[block->used_size++] = value;\
-		return INSERT_SUCCESS;\
-	}\
-}\
-enum anyblock_delete_codes delete_from_##name##_block(struct name##_block * block, type value) {\
-	bool del_performed = false;\
-	for (int block_index = block->used_size - 1; block_index >= 0; block_index--) {\
-	if (name##_required_equal(block->data[block_index], value)) {\
-			printf("DELETING\n");\
-			memmove(\
-				&block->data[block_index],\
-				&block->data[block_index + 1],\
-				sizeof(type) * ((block->used_size--) - (block_index + 1))\
-			);\
-			del_performed = true;\
-		}\
-	}\
-	return del_performed ? DELETE_SUCCESS : DELETE_NOT_FOUND;\
-}\
 
-*/
+enum anyblock_insert_codes insert_into_block(void * block_data, struct block_metadata * meta, void * value);
+
+enum anyblock_delete_codes delete_from_block(void * block_data, struct block_metadata * meta, void * value, bool (* equal)(void *, void *));
+
 
 #endif /* block_h */
